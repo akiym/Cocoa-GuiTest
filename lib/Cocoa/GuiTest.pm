66c239ec91d25058db2e972ace4ce46ef80fe056
Takumi Akiyama <t.akiym@gmail.com>
2012-08-15 22:34:30 +0900

    initial commit

---
+++ lib/Cocoa/GuiTest.pm
@@ -0,0 +1,32 @@
+package Cocoa::GuiTest;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Cocoa::GuiTest -
+
+=head1 SYNOPSIS
+
+  use Cocoa::GuiTest;
+
+=head1 DESCRIPTION
+
+Cocoa::GuiTest is
+
+=head1 AUTHOR
+
+Takumi Akiyama E<lt>t.akiym at gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
